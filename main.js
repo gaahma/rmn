e3d065babcf2a270b0da910cea7be33639386811
Haag, Adam (ah616j) <ah616j@att.com>
2018-11-21 20:17:09 -0500

    saving

--- main.js
+++ main.js
@@ -2,28 +2,45 @@ const Rule_Set = require('./models/Rule_Set');
 const Calendar = require('./models/Calendar');
 const moment = require('moment');
 const File_Handler = require('./File_Handler/File_Handler');
-const rule_params = require('./rule_sets/my_rules.js');
+
 const calendar = new Calendar();
 // const fh = new File_Handler();
 
-const my_rules = rule_params.map(rule => new Rule_Set(rule));
-let epoch = moment().format('YYYY-MM-DD');
+const my_rules = 
+    require('./rule_sets/my_rules.js').map(rule => new Rule_Set(rule));
+
+let date = moment().format('YYYY-MM-DD');
 
 // console.log(my_rules);
-const timeline = [epoch];
+const timeline = [];
 for (let i = 0; i< 10; i++){
-    epoch = moment(epoch).add(1, 'day').format('YYYY-MM-DD');
-    timeline.push(epoch);
+    date = moment(date).add(1, 'day').format('YYYY-MM-DD');
+    timeline.push(date);
 }
+const transactions = {};
 
 timeline.forEach(date => {
     const day = calendar.get(date);
     my_rules.forEach(rule => {
+        const applicable_date = calendar.get_applicable_date(day, rule);
         if(rule.matches(day)){
-            console.log(day.epoch);
+            if(!transactions.hasOwnProperty(applicable_date)){
+                transactions[applicable_date] = [];
+            }
+            transactions[applicable_date].push(rule.transaction);
         }
-    })
-})
+    });
+});
+let report = "";
+timeline.forEach(date => {
+    report += date +
+        "\n" +
+        `${transactions[date].name} : ${transactions[date].amt}\n\n`
+});
+
+console.log(report);
+
+
 
 
 
